d7f510dc421701beec8670dd0a705289681c76c6
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-09 17:49:48 +0800

    Import usePathname to the nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,8 @@
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+'use client'
+
+import {DocumentDuplicateIcon, HomeIcon, UserGroupIcon,} from '@heroicons/react/24/outline';
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -18,6 +17,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+    const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {

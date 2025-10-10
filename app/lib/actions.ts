c730811e31fcc8aba6c40180b704a386cc240f5b
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-10 21:36:10 +0800

    Change customerId validation to string

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,7 +9,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.coerce.number(),
+    customerId: z.string(),
     amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
     date: z.string(),

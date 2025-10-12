18ef2f882a4a53c92ec04804fb7fe1722b9731de
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-12 21:57:40 +0800

    In the edit invoice form, update the invoice status error message

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -100,6 +100,7 @@ export default function EditInvoiceForm({
                                     value="pending"
                                     defaultChecked={invoice.status === 'pending'}
                                     className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                                    aria-describedby="invoice-status-error"
                                 />
                                 <label
                                     htmlFor="pending"
@@ -116,6 +117,7 @@ export default function EditInvoiceForm({
                                     value="paid"
                                     defaultChecked={invoice.status === 'paid'}
                                     className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                                    aria-describedby="invoice-status-error"
                                 />
                                 <label
                                     htmlFor="paid"
@@ -125,6 +127,13 @@ export default function EditInvoiceForm({
                                 </label>
                             </div>
                         </div>
+                    </div>
+                    <div id="invoice-status-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.status && state.errors?.status.map((error) => (
+                            <p className="mt-2 text-sm text-red-500">
+                                {error}
+                            </p>
+                        ))}
                     </div>
                 </fieldset>
             </div>

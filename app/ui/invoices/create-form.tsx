3f45bb475f377a648e76c0bb80495e31c76b1fe7
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-12 21:26:28 +0800

    Add error message for the invoice number field

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -49,10 +49,10 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
         {/* Invoice Amount */}
         <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
-          </label>
-          <div className="relative mt-2 rounded-md">
+            <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+                Choose an amount
+            </label>
+            <div className="relative mt-2 rounded-md">
             <div className="relative">
               <input
                 id="amount"

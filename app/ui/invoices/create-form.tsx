ca71eb2122421d811c7f8f9067373e6c29802ca9
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-12 22:18:52 +0800

    Refactor with ErrorField component

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -68,13 +68,7 @@ export default function Form({customers}: { customers: CustomerField[] }) {
                                 className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
                         </div>
                     </div>
-                    <div id="invoice-amount-error" aria-live="polite" aria-atomic="true">
-                        {state.errors?.amount && state.errors?.amount.map((error: string) => (
-                            <p className="mt-2 text-sm text-red-500">
-                                {error}
-                            </p>
-                        ))}
-                    </div>
+                    <ErrorField errorFieldId="invoice-amount-error" errorMessage={state.errors?.amount?.[0]} />
                 </div>
 
                 {/* Invoice Status */}

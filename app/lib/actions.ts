2606951328f3f5125e5a69e84dfeb749a9e7f5b7
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-12 15:55:07 +0800

    Add error.tsx for the invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,7 +72,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
     } catch (error) {

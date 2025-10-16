b91301b9f01520cd0e4aa75fe814cc045e8a84c5
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-16 14:30:43 +0800

    Add SQL and bcrypt library

--- auth.ts
+++ auth.ts
@@ -1,8 +1,20 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import {authConfig} from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import {z} from 'zod';
+import type {User} from '@/app/lib/definitions'
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
-    providers: [Credentials({})],
+    providers: [Credentials({
+        async authorize(credentials) {
+            const parsedCredentials = z
+                .object({email: z.string().email(), password: z.string().min(6)})
+                .safeParse(credentials);
+        },
+    })],
 });

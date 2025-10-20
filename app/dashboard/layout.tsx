5a849be0ce3ea1ccc448d242f2df4ffc84a1353b
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-10-20 19:30:15 +0800

    Add title for the customer page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import {Metadata} from "next";
 
 export const metadata: Metadata = {
     title: {
-        template: '%s | Dashboard',
+        template: '%s | Acme Dashboard',
         default: 'Dashboard',
     },
 }
